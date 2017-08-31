23a093c62b0417de58d0ab03ab6edbd94efb1bb6
windsor80 <windsorjunior@hotmail.com>
2017-08-30 23:39:28 -0400

    updates

--- app/todoList/features/create/create.controller.js
+++ app/todoList/features/create/create.controller.js
@@ -6,12 +6,21 @@ angular
     // uppercase first letter, Ctrl suffix)
 
 function CreateCtrl($scope, $http) {
+	// vm stands for View Model - anything on "the vm" 
+    // is exposed to the view
+
+    // The Controller is the BRAINS of the module
+    // before the view renders, the controller is ran
+    // this function is ran, so whatever you want to
+    // happen before the State shows up, goes in this function
+    
     var vm = this; 
 
     vm.taskName = '';
     vm.dueDate = '';
     vm.opinion = '';
 
+    // watch variable change to display new content on page
     $scope.$watch(function(){
     	return vm.taskName;
     }, function(newValue, oldValue) {
@@ -23,12 +32,5 @@ function CreateCtrl($scope, $http) {
     		vm.opinion = "BORING";
     	}
     })
-    // vm stands for View Model - anything on "the vm" 
-    // is exposed to the view
-
-    // The Controller is the BRAINS of the module
-    // before the view renders, the controller is ran
-    // this function is ran, so whatever you want to
-    // happen before the State shows up, goes in this function
     
 }
--- app/todoList/todoList.module.js
+++ app/todoList/todoList.module.js
@@ -15,4 +15,5 @@ function appConfig($urlRouterProvider) {
 
     // If the URL does not exist as a route, redirect to /
     $urlRouterProvider.otherwise('/create');
+    // Route of the state where the user lands when visits page
 }
