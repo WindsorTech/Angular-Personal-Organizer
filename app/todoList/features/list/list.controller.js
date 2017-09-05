11eafb7ba53e848ba75c5e54fa1a143da0fc0b04
windsor80 <windsorjunior@hotmail.com>
2017-09-05 12:57:53 -0400

    updates

--- app/todoList/features/list/list.controller.js
+++
@@ -1,9 +0,0 @@
-angular
-    .module('todoList.list') // No array, as we are "extending" the module
-    .controller('ListCtrl', ListCtrl); // Define our controller (Notice the naming convention - uppercase first letter, Ctrl suffix)
-
-function ListCtrl($stateParams) {
-    var vm = this; // vm stands for View Model - anything on "the vm" is exposed to the view
-
-    
-}
