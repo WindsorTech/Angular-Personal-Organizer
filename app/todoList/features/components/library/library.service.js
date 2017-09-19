b935c5ece3c05f136209bc2d2890fde3433b6cde
windsor80 <windsorjunior@hotmail.com>
2017-09-19 01:03:54 -0400

    updates

--- app/todoList/features/components/library/library.service.js
+++ app/todoList/features/components/library/library.service.js
@@ -7,22 +7,6 @@ function LibraryService() {
 
 	var tasks = [];
 
-	//  var tasks = [
-	//  	{
-	// 		taskName: 'Karate',
-	// 		dueDate: '08/08/2008'
-	// 	},
-	// 	{
-	// 		taskName: 'Eat Pussy',
-	// 		dueDate: '11/11/2011'
-	// 	},
-	// 	{
-	// 		taskName: 'Go to the Gym',
-	// 		dueDate: 'NOW!!'
-	// 	}
-
-	// ]
-
 	function addTask(taskData) {
 		tasks.push(taskData);
 	}
