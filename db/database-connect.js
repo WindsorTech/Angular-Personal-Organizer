c5a504d6f54d54c7f33fd3b9b83dadf7e6cf47f4
windsor80 <windsorjunior@hotmail.com>
2017-10-08 22:11:50 -0400

    updates

--- db/database-connect.js
+++ db/database-connect.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-//mongoose.Promise = Promise;
+mongoose.Promise = Promise;
 
 mongoose.connect("mongodb://heroku_8mkc7nl7:5j8nnk4ta9lpih6ktmatsg1cqa@ds149144.mlab.com:49144/heroku_8mkc7nl7");
 
